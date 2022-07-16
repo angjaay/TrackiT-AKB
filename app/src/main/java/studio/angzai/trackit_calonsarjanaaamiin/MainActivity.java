06139ed60d2e9c16a1f42037704536318f057d66
angjaay <angga.angzai@gmail.com>
2022-07-16 20:57:41 +0700

    first commit

---
+++ app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/MainActivity.java
@@ -0,0 +1,14 @@
+package studio.angzai.trackit_calonsarjanaaamiin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
