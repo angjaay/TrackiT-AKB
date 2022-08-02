fda6244d52d1438b1b347979cd5f724a0dcf4636
primarazaq <primarazaq34@gmail.com>
2022-08-03 00:42:39 +0700

    retrieving data from firebase to recyclerview

--- app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/adapter/RecyclerViewConfig.java
+++ app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/adapter/RecyclerViewConfig.java
@@ -1,9 +1,9 @@
 package studio.angzai.trackit_calonsarjanaaamiin.adapter;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,8 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-
-import java.util.List;
 
 import studio.angzai.trackit_calonsarjanaaamiin.R;
 import studio.angzai.trackit_calonsarjanaaamiin.model.Place;
@@ -25,6 +23,8 @@ public class RecyclerViewConfig extends FirebaseRecyclerAdapter<Place, RecyclerV
      *
      * @param options
      */
+
+
     public RecyclerViewConfig(@NonNull FirebaseRecyclerOptions<Place> options) {
         super(options);
     }
@@ -32,11 +32,12 @@ public class RecyclerViewConfig extends FirebaseRecyclerAdapter<Place, RecyclerV
     @Override
     protected void onBindViewHolder(@NonNull PlaceAdapter holder, int position, @NonNull Place model) {
         holder.txtname.setText(model.getName());
-        holder.imageTempat.setText(model.getImage());
-        holder.txtdescription.setText(model.getDescription());
-        holder.latitudetempat.setText(String.valueOf(model.getLatitude()));
-        holder.longitudetempat.setText(String.valueOf(model.getLongitude()));
-        holder.txtopentime.setText(String.valueOf(model.getOpen_time()));
+        //holder.imageTempat.setText(model.getImage());
+        //holder.imageTempat.setBackgroundResource(R.drawable.tangkubanperahu);
+        //holder.txtdescription.setText(model.getDescription());
+        //holder.latitudetempat.setText(String.valueOf(model.getLatitude()));
+        //holder.longitudetempat.setText(String.valueOf(model.getLongitude()));
+        //holder.txtopentime.setText(String.valueOf(model.getOpen_time()));
         holder.txtscore.setText(String.valueOf(model.getScore()));
         holder.txtaddress.setText(model.getAddress());
     }
@@ -44,7 +45,7 @@ public class RecyclerViewConfig extends FirebaseRecyclerAdapter<Place, RecyclerV
     @NonNull
     @Override
     public PlaceAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_place, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.places_list_item, parent, false);
 
         return new PlaceAdapter(view);
     }
@@ -52,17 +53,21 @@ public class RecyclerViewConfig extends FirebaseRecyclerAdapter<Place, RecyclerV
     public static class PlaceAdapter extends RecyclerView.ViewHolder
     {
         TextView txtname, txtaddress, txtdescription, imageTempat,latitudetempat, longitudetempat, txtopentime, txtscore;
+        ImageView images;
 
         public PlaceAdapter(@NonNull View itemView) {
             super(itemView);
-            txtname = itemView.findViewById(R.id.textName);
-            txtaddress = itemView.findViewById(R.id.textAddress);
-            txtdescription = itemView.findViewById(R.id.textDescription);
-            imageTempat = itemView.findViewById(R.id.imageTempat);
-            latitudetempat = itemView.findViewById(R.id.latitudeTempat);
-            longitudetempat = itemView.findViewById(R.id.longitudeTempat);
-            txtopentime = itemView.findViewById(R.id.textOpenTime);
-            txtscore = itemView.findViewById(R.id.textScore);
+            txtname = itemView.findViewById(R.id.txtname);
+            txtaddress = itemView.findViewById(R.id.txtaddress);
+            images = itemView.findViewById(R.id.txtimage);
+            //txtdescription = itemView.findViewById(R.id.textDescription);
+            //imageTempat = ImageView.findViewById(R.id.txtimage);
+            //latitudetempat = itemView.findViewById(R.id.latitudeTempat);
+            //longitudetempat = itemView.findViewById(R.id.longitudeTempat);
+            //txtopentime = itemView.findViewById(R.id.textOpenTime);
+            txtscore = itemView.findViewById(R.id.txtrating);
         }
+
+
     }
 }
