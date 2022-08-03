4360afc6b734537dc33955f456b27e30617595c2
primarazaq <primarazaq34@gmail.com>
2022-08-04 03:21:53 +0700

    passing data from homefragment to detailfragment

--- app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/DetailFragment.java
+++ app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/DetailFragment.java
@@ -4,21 +4,58 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 public class DetailFragment extends Fragment {
 
+    String name,address,image,description,time;
+    float latitude,longitude,score;
+
+
     public DetailFragment() {
         // Required empty public constructor
+    }
+
+    public DetailFragment(String name, String image, String address, float latitude, float longitude, String description, float score, String time) {
+        this.name = name;
+        this.address = address;
+        this.image = image;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.description = description;
+        this.score = score;
+        this.time = time;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        ImageView imageholder = view.findViewById(R.id.img_tempat);
+        TextView nameholder = view.findViewById(R.id.name_tempat);
+        TextView addressholder = view.findViewById(R.id.address_tempat);
+        TextView descholder = view.findViewById(R.id.desc_tempat);
+        TextView timeholder = view.findViewById(R.id.time_tempat);
+
+        nameholder.setText(name);
+        addressholder.setText(address);
+        descholder.setText(description);
+        timeholder.setText(time);
+        Glide.with(getContext()).load(image).into(imageholder);
+
+        return view;
     }
 
-
+    public void onBackPressed(){
+        AppCompatActivity activity =(AppCompatActivity)getContext();
+        activity.getSupportFragmentManager().beginTransaction().replace(R.id.wrapper, new HomeFragment()).addToBackStack(null).commit();
+    }
 }
--- app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/adapter/RecyclerViewConfig.java
+++ app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/adapter/RecyclerViewConfig.java
@@ -7,12 +7,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
+import studio.angzai.trackit_calonsarjanaaamiin.DetailFragment;
 import studio.angzai.trackit_calonsarjanaaamiin.R;
 import studio.angzai.trackit_calonsarjanaaamiin.model.Place;
 
@@ -40,6 +42,21 @@ public class RecyclerViewConfig extends FirebaseRecyclerAdapter<Place, RecyclerV
         holder.txtscore.setText(String.valueOf(model.getScore()));
         holder.txtaddress.setText(model.getAddress());
         Glide.with(holder.images.getContext()).load(model.getImage()).into(holder.images);
+
+            holder.images.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AppCompatActivity activity =(AppCompatActivity)view.getContext();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.wrapper, new DetailFragment(model.getName(),
+                                                                                                                     model.getImage(),
+                                                                                                                     model.getAddress(),
+                                                                                                                     model.getLatitude(),
+                                                                                                                     model.getLongitude(),
+                                                                                                                     model.getDescription(),
+                                                                                                                     model.getScore(),
+                                                                                                                     model.getOpen_time())).addToBackStack(null).commit();
+                }
+            });
     }
 
     @NonNull
