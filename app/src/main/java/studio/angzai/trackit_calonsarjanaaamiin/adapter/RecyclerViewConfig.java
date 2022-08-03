c5c6200e6bf9e83bee232b238d5d08cdef239c3c
Reichan Muhammad <reichan.muhammad@gmail.com>
2022-08-03 13:51:33 +0700

    Adding Read Image

--- app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/adapter/RecyclerViewConfig.java
+++ app/src/main/java/studio/angzai/trackit_calonsarjanaaamiin/adapter/RecyclerViewConfig.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
@@ -32,40 +33,38 @@ public class RecyclerViewConfig extends FirebaseRecyclerAdapter<Place, RecyclerV
     @Override
     protected void onBindViewHolder(@NonNull PlaceAdapter holder, int position, @NonNull Place model) {
         holder.txtname.setText(model.getName());
-        //holder.imageTempat.setText(model.getImage());
-        //holder.imageTempat.setBackgroundResource(R.drawable.tangkubanperahu);
-        //holder.txtdescription.setText(model.getDescription());
-        //holder.latitudetempat.setText(String.valueOf(model.getLatitude()));
-        //holder.longitudetempat.setText(String.valueOf(model.getLongitude()));
-        //holder.txtopentime.setText(String.valueOf(model.getOpen_time()));
+        holder.txtdescription.setText(model.getDescription());
+        holder.latitudetempat.setText(String.valueOf(model.getLatitude()));
+        holder.longitudetempat.setText(String.valueOf(model.getLongitude()));
+        holder.txtopentime.setText(String.valueOf(model.getOpen_time()));
         holder.txtscore.setText(String.valueOf(model.getScore()));
         holder.txtaddress.setText(model.getAddress());
+        Glide.with(holder.images.getContext()).load(model.getImage()).into(holder.images);
     }
 
     @NonNull
     @Override
     public PlaceAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.places_list_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_place, parent, false);
 
         return new PlaceAdapter(view);
     }
 
     public static class PlaceAdapter extends RecyclerView.ViewHolder
     {
-        TextView txtname, txtaddress, txtdescription, imageTempat,latitudetempat, longitudetempat, txtopentime, txtscore;
+        TextView txtname, txtaddress, txtdescription,latitudetempat, longitudetempat, txtopentime, txtscore;
         ImageView images;
 
         public PlaceAdapter(@NonNull View itemView) {
             super(itemView);
-            txtname = itemView.findViewById(R.id.txtname);
-            txtaddress = itemView.findViewById(R.id.txtaddress);
-            images = itemView.findViewById(R.id.txtimage);
-            //txtdescription = itemView.findViewById(R.id.textDescription);
-            //imageTempat = ImageView.findViewById(R.id.txtimage);
-            //latitudetempat = itemView.findViewById(R.id.latitudeTempat);
-            //longitudetempat = itemView.findViewById(R.id.longitudeTempat);
-            //txtopentime = itemView.findViewById(R.id.textOpenTime);
-            txtscore = itemView.findViewById(R.id.txtrating);
+            txtname = itemView.findViewById(R.id.textName);
+            txtaddress = itemView.findViewById(R.id.textAddress);
+            images = itemView.findViewById(R.id.imageTempat);
+            txtdescription = itemView.findViewById(R.id.textDescription);
+            latitudetempat = itemView.findViewById(R.id.latitudeTempat);
+            longitudetempat = itemView.findViewById(R.id.longitudeTempat);
+            txtopentime = itemView.findViewById(R.id.textOpenTime);
+            txtscore = itemView.findViewById(R.id.textScore);
         }
 
 
